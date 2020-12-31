74393a905a91f2528ea106dea1aa1b765078c732
Devildk384 <durgeshkumar384@gmail.com>
2020-12-31 18:06:40 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,66 +2,97 @@ import RecordRTC from "recordrtc";
 import './App.css';
 
 function App() {
-  var video = document.querySelector("video");
-  var recorder; 
+    let videoElm = document.querySelector("video");
+    
+    let flipBtn = document.querySelector('#flip-btn');
 
-
-  const captureCamera = (callback) => {
-      navigator.mediaDevices.getUserMedia({ audio: true, video: true }).then(function(camera) {
-          callback(camera);
-          
-      }).catch(function(error) {
-          alert('camera not found');
-          console.error(error);
+    var recorder; 
+    
+    
+    let defaultsOpts = { audio: true, video: true }
+    let shouldFaceUser = true;
+    
+   
+    let supports = navigator.mediaDevices.getSupportedConstraints();
+    if( supports['facingMode'] === true ) {
+      flipBtn.disabled = false;
+    }
+    
+    let stream = null;
+    
+    function capture(callback) {
+      defaultsOpts.video = { facingMode: shouldFaceUser ? 'user' : 'environment' }
+      navigator.mediaDevices.getUserMedia(defaultsOpts)
+        .then(function(_stream) {
+          stream = _stream;
+          videoElm.srcObject = stream;
+          videoElm.play();
+          callback(stream)
+        })
+        .catch(function(err) {
+          console.log(err)
+        });
+    }
+    
+    flipBtn.addEventListener('click', function(){
+      if( stream == null ) return
+      stream.getTracks().forEach(t => {
+        t.stop();
       });
-      console.log(video);
-  }
-  
-  const  stopRecordingCallback= () =>{
-      video.src = video.srcObject = null;
-      video.muted = false;
-      video.volume = 1;
-      video.src = URL.createObjectURL(recorder.getBlob());
+      shouldFaceUser = !shouldFaceUser;
+      capture();
+    })
+
       
-      recorder.camera.stop();
-      recorder.destroy();
-      recorder = null;
-  }
-  
+    
+    
+    const startVideo = () => {
+        capture();
+    }
 
-   const startRecordingCall = () =>{
-     
-     
-      captureCamera(function(camera) {
+    const  stopRecordingCallback= () =>{
+        videoElm.src = videoElm.srcObject = null;
+        videoElm.muted = false;
+        videoElm.volume = 1;
+        videoElm.src = URL.createObjectURL(recorder.getBlob());
+        
+        recorder.stream.stop();
+        recorder.destroy();
+        recorder = null;
+    }
 
-          video.volume = 1;
-          video.srcObject = camera;
+    const startRecordingCall = () =>{
+        capture(function(stream) {
   
-          recorder = RecordRTC(camera, {
-              type: 'video'
-          });
-  
-          recorder.startRecording();
-  
-          recorder.camera = camera;
-         
-      });
-  };
-  
- const  stopRecording = () => { 
-      recorder.stopRecording(stopRecordingCallback);
-  };
-  
+            videoElm.volume = 0;
+            videoElm.srcObject = stream;
+    
+            recorder = RecordRTC(stream, {
+                type: 'video'
+            });
+    
+            recorder.startRecording();
+    
+            recorder.stream = stream;
+           
+        });
+    };
+
+    const  stopRecording = () => { 
+        recorder.stopRecording(stopRecordingCallback);
+    };
+    
 
   return (
-    <div className="App">
+    <div className="App"> 
     
     <title>Video Recording</title>
      <h1>Record and download </h1>
      <br></br>
  
-   <button onClick={startRecordingCall}  type="button">Start Recording</button>
-   <button  onClick={stopRecording}   type="button" > stopRecording</button>
+   <button onClick={startVideo}  type="button">Start Camera</button>
+   <button onClick={startRecordingCall} type="button">Start Recording</button>
+   <button onClick={stopRecording}  type="button" > stopRecording</button>
 
      
     </div>
